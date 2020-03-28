3d20dd12c3e8846d55c7b51aa6e23436275d140d
Max Wagner <max@trollbu.de>
2020-03-28 13:00:28 +0100

    Put a Buffer around document input streams
    Jesus, Java standard library, get your shit together.

--- src/main/java/DocumentServlet.java
+++ src/main/java/DocumentServlet.java
@@ -3,9 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -39,9 +37,11 @@ public class DocumentServlet extends HttpServlet {
         byte[] content = new byte[total];
         int read = 0;
         int available = 0;
-        // I'm not using a BufferedReader for this, since the OS is keeping a buffer for all the data
-        // that has been received anyways.
-        InputStream is = req.getInputStream();
+        // apparently, InputStream.read(byte[], offset, length) doesn't simply call down to memcpy,
+        // but instead opts to *read the bytes one at a time*, so I guess we're slapping
+        // a BufferedInputStream around this. This does double the in-transit memory footprint
+        // of a document, but the alternative would be reimplementing what InputStream *should* be doing.
+        BufferedInputStream is = new BufferedInputStream(req.getInputStream(), total);
         while (read < total) {
             // ensure Blocking read, rather than burning CPU time
             available = Math.max(is.available(), 1);
@@ -77,9 +77,17 @@ public class DocumentServlet extends HttpServlet {
             // user is trying to GET /storage/documents, which only accepts POST
             resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
         }
+        String uuid = req.getPathInfo().substring(1);  // strip '/' at beginning
+        if (!this.documents.containsKey(uuid)) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
 
-        PrintWriter out = resp.getWriter();
-        out.println("HERE BE DRAGONS!\n");
-        out.println(req.getPathInfo() + "\n");
+        resp.setContentType(this.contentTypes.get(uuid));
+        byte[] doc = this.documents.get(uuid);
+        resp.setContentLength(doc.length);
+
+        BufferedOutputStream out = new BufferedOutputStream(resp.getOutputStream());
+        out.write(doc);
+
     }
 }
